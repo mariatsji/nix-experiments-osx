efa28f5376ae2b0ca1e1912b837cdb046ef95e31
Sjur Millidahl <sjur.millidahl@finn.no>
2020-07-12 16:29:37 +0200

    nix-build of shell.nix now works at the same time as stack build works omgomgomgomg

--- my-stack-shell-nix/README.md
+++ my-stack-shell-nix/README.md
@@ -1,3 +1,5 @@
 	nix-channel --add https://nixos.org/channels/nixpkgs-19.09-darwin nixpkgs && nix-channel --update
 
 	stack build
+
+	nix-build shell.nix
--- my-stack-shell-nix/shell.nix
+++ my-stack-shell-nix/shell.nix
@@ -1,8 +1,9 @@
-{ghc}:
+{ghc ? with import <nixpkgs> {}; haskell.compiler.ghc882}:
 with (import <nixpkgs> {});
 
 haskell.lib.buildStackProject {
   inherit ghc;
   name = "my-stack-shell-nix";
   buildInputs = [ glpk pcre ];
+  src = ./.;
 }
