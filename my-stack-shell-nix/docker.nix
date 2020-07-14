93d7aa06c813efe4db58f760fbe2e2f7b7d7227f
Sjur Millidahl <sjur.millidahl@finn.no>
2020-07-14 09:38:15 +0200

    building in docker seems to work soon

--- my-stack-shell-nix/docker.nix
+++ my-stack-shell-nix/docker.nix
@@ -1,9 +1,8 @@
-let artoFacto = import ./shell.nix;
+let artoFacto = import ./shell.nix {};
     pkgs = import <nixpkgs> {};
-    linuxpkgs = import <nixpkgs> { system = "x86_64-linux"; };
 
 in pkgs.dockerTools.buildImage {
   name = "my-stack-shell-nix";
   tag = "latest";
-  contents = [ linuxpkgs.bashInteractive ];
+  contents = [ artoFacto ];
 } 
